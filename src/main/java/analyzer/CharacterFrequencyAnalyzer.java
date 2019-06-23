1e4899306e5a0132b8fe11dc5babde0f7db9c8e2
nickl0gist <nicklogistick@mail.ru>
2019-06-23 15:04:06 +0200

    Charachter Frequency implemented

--- src/main/java/analyzer/CharacterFrequencyAnalyzer.java
+++ src/main/java/analyzer/CharacterFrequencyAnalyzer.java
@@ -1,18 +1,48 @@
 package analyzer;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class CharacterFrequencyAnalyzer implements Analyzer <Map <String, Integer>>{
-
+public class CharacterFrequencyAnalyzer implements Analyzer <Map <Character, Double>>{
 
     @Override
-    public Map <String, Integer> analyze(String text) {
-        return null;
+    public Map <Character, Double> analyze(String text) {
+        Map<Character, Long> map = new HashMap<>();
+        String[] arr = Analyzer.getTextArray(text);
+
+        map = String.join(" ", arr)
+                .chars()
+                .mapToObj(x -> (char) x)
+                .map(c -> c.toString().toLowerCase())
+                .filter(c -> c.matches("\\p{L}"))
+                .map(x -> x.charAt(0))
+                .collect(Collectors.groupingBy(x->x, Collectors.counting()));
+
+        Long total_value = map.values()
+                .stream()
+                .reduce(Long::sum)
+                .orElseThrow(IllegalArgumentException::new);
+
+        return  map
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(e -> e.getKey(), e -> (e.getValue().doubleValue() / total_value) * 100.0));
     }
 
     @Override
-    public String interpret(Map <String, Integer> analyzeResults) {
-        return null;
+    public String interpret(Map <Character, Double> analyzeResults) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Map.Entry<Character, Double> entry : analyzeResults.entrySet()) {
+            stringBuilder.append("[ '");
+            stringBuilder.append(entry.getKey());
+            stringBuilder.append("' => ");
+            stringBuilder.append(String.format("%.2f", entry.getValue()));
+            stringBuilder.append(" ]");
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
---
+++ src/test/java/analyzer/CharacterFrequencyAnalyzerTest.java
@@ -0,0 +1,33 @@
+package analyzer;
+
+import org.junit.Before;
+
+import org.junit.Before;
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+public class CharacterFrequencyAnalyzerTest {
+
+    CharacterFrequencyAnalyzer analyzer;
+
+    @Before
+    public void setUp() {
+        this.analyzer = new CharacterFrequencyAnalyzer();
+    }
+
+    @Test
+    public void bigerTextAnalyze(){
+        String text = "Lorem Ipsum";
+        String result ="[ 'p' => 10,00 ]\n" +
+                "[ 'r' => 10,00 ]\n" +
+                "[ 's' => 10,00 ]\n" +
+                "[ 'u' => 10,00 ]\n" +
+                "[ 'e' => 10,00 ]\n" +
+                "[ 'i' => 10,00 ]\n" +
+                "[ 'l' => 10,00 ]\n" +
+                "[ 'm' => 20,00 ]\n" +
+                "[ 'o' => 10,00 ]\n";
+        //System.out.println(analyzer.interpret(analyzer.analyze(text)));
+        assertEquals(result, analyzer.interpret(analyzer.analyze(text)));
+    }
+}
